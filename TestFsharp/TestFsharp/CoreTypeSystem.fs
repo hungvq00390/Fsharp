4f2a943399ab16d2d90a91f56399f10de0032758
hungvq00390 <hungvq00390@gmail.com>
2015-05-02 09:07:57 +0700

    Fix Merge, fix jc

--- TestFsharp/TestFsharp/CoreTypeSystem.fs
+++ TestFsharp/TestFsharp/CoreTypeSystem.fs
@@ -38,7 +38,7 @@ let rec join (lst: TagSeq) : TagSeq =
 
 // Define Merge function
 let rec merge (lst1: TagSeq) (lst2: TagSeq) : TagSeq = 
-    if lst1.Length <= 0 || lst2.Length <= 0 then [] else
+    if List.isEmpty(lst1) then lst2 else if List.isEmpty(lst2) then lst1 else
     let tag1 = fst (List.head lst1) in
     let tag2 = fst (List.head lst2) in
     if tag1 = Tag.Max && tag2 = Tag.Max then (Tag.Max, (snd (List.head lst1)) + (snd (List.head lst2))) :: (merge (List.tail lst1) (List.tail lst2))
@@ -53,8 +53,12 @@ let rec jc (lst1: TagSeq) (lst2: TagSeq) : TagSeq =
     | [] -> []
     | (Tag.Plus,m1)::[] -> match lst2 with
                            | [] -> []
-                           | (Tag.Max,n1)::(Tag.Join,n2)::xs2 -> if m1 > 1 then jc [(Tag.Plus,m1-1)] (seq((Tag.Max,n1+n2) :: xs2)) else (Tag.Max,n1+n2) :: xs2
+                           | (Tag.Max,l1)::(Tag.Join,l2)::xs2 -> if m1 > 1 then jc [(Tag.Plus,m1-1)] (seq((Tag.Max,l1+l2) :: xs2)) else (Tag.Max,l1+l2) :: xs2
                            | x::xs -> []
+    | (Tag.Plus,n1)::(Tag.Max,n2)::[] -> match lst2 with
+                                         | [] -> []
+                                         | (Tag.Max,l1)::(Tag.Join,l2)::xs2 -> if n1 > 1 then jc ((Tag.Plus,(n1-1)) :: [Tag.Join,(max (n2+1) (l1+l2))]) xs2 else jc [Tag.Join,max (n2+1) (l1+l2)] xs2
+                                         | x::xs -> [] 
     | x::xs -> []
 
 // Define Choice function
