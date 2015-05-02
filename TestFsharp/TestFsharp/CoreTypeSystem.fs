d01b074cad9765d6b806c66420529ef80b18d464
hungvq00390 <hungvq00390@gmail.com>
2015-05-02 08:17:48 +0700

    Clean Code, Make new Test file

--- TestFsharp/TestFsharp/CoreTypeSystem.fs
+++ TestFsharp/TestFsharp/CoreTypeSystem.fs
@@ -10,12 +10,6 @@ type TagNum = Tag * int
 
 type TagSeq = TagNum list
 
-let rec max (a:int) (b:int) = if a > b then a else b
-let rec append (lst1: TagSeq) (lst2: TagSeq): TagSeq =
-   match lst1 with 
-   | [] -> []
-   | hd :: tl -> hd :: append tl lst2
-
 let e55 = [(Tag.Plus,1); (Tag.Plus,1); (Tag.Minus,3);(Tag.Plus,1);(Tag.Minus,1);(Tag.Plus,1);(Tag.Minus,1);]
 
 // Define seq function
@@ -48,7 +42,7 @@ let rec Canonical (lst1: TagSeq) : TagSeq =
                                                      else (Tag.Max, n1)::Canonical xs 
                               else (Tag.Max, n1)::[]
     | (Tag.Plus, n1)::xs -> if (xs.Length > 0) then if fst (List.head xs) = Tag.Plus then Canonical ((Tag.Plus, n1 + (snd (List.head xs)))::(List.tail xs)) 
-                                                       else if (xs.Length > 1) then if fst (List.head xs) = Tag.Max && fst (List.head(List.tail xs)) = Tag.Minus then Canonical (append (CanonicalTemp (Tag.Plus, n1) (List.head xs) (List.head(List.tail xs))) (List.tail xs))
+                                                       else if (xs.Length > 1) then if fst (List.head xs) = Tag.Max && fst (List.head(List.tail xs)) = Tag.Minus then Canonical (List.append (CanonicalTemp (Tag.Plus, n1) (List.head xs) (List.head(List.tail xs))) (List.tail xs))
                                                                                         else (Tag.Plus, n1)::Canonical xs
                                                             else xs
                                else (Tag.Plus, n1)::[]
@@ -107,78 +101,3 @@ let rec choice (lst1: TagSeq) (lst2: TagSeq) : TagSeq =
                | [] -> []
                | x2::xs2 -> if (fst x) = (fst x2) && (xs = xs2) && (fst x) = Tag.Max then if (snd x >= snd x2) then lst1 else lst2
                               else []
-
-let rec getNextTag (lst1: TagSeq) : TagSeq =
-    match lst1 with
-    | [] -> []
-    | x::xs -> [fst x,1]
-
-///// Test Mainfunction /////
-let isTag tag = if (tag = '+' || tag = '-' || tag = '#' || tag = ':') then true else false
-let rec removeValue (chrlst:char list) =
- match chrlst with
- | [] -> []
- | x::xs -> if (isTag x) then x::xs else removeValue xs
-
-let rec getValue (chrlst:char list) =
- match chrlst with
- | [] -> []
- | x::xs -> if (isTag x) then [] else x::getValue xs
-
-let string2charlist (str:string) = [for c in str -> c]
-let rec charlist2stringlist (s:char list) = 
- match s with 
- | [] -> []
- | x::xs -> x.ToString()::charlist2stringlist xs
-let rec stringToInt str = System.Int32.Parse(str |> String.concat "")
-let rec charListToInt str = System.Int32.Parse(charlist2stringlist(str) |> String.concat "")
-let rec charList2String s = charlist2stringlist(s) |> String.concat ""
-
-let rec charList2type (chrlst:char list) : TagSeq=
- match chrlst with
- | [] -> []
- | '+'::xs -> (Tag.Plus, charListToInt(getValue xs)):: charList2type (removeValue xs)
- | '-'::xs -> (Tag.Minus, charListToInt(getValue xs)):: charList2type (removeValue xs)
- | '#'::xs -> (Tag.Max, charListToInt(getValue xs)):: charList2type (removeValue xs)
- | ':'::xs -> (Tag.Join, charListToInt(getValue xs)):: charList2type (removeValue xs)
- | x::xs -> charList2type xs
-
-let rec type2string (lst:TagSeq) : string =
- match lst with
- | [] -> ""
- | (Tag.Plus, n1)::xs -> "+" + n1.ToString() + type2string xs
- | (Tag.Minus, n1)::xs -> "-" + n1.ToString() + type2string xs
- | (Tag.Max, n1)::xs -> "#" + n1.ToString() + type2string xs
- | (Tag.Join, n1)::xs -> ":" + n1.ToString() + type2string xs
- | x::xs -> x.ToString() + type2string xs
-
-let rec string2Type str : TagSeq = charList2type (string2charlist str)
-
-///// Test /////
-let s13 = "-1#2-3+2+22-3-2";;
-printfn "Test Join %A" (type2string (join (string2Type s13)));
-printfn "Test e55 %A" (type2string e55);
-
-let s11 = "#1#2:3";;
-let s12 = "#1#2:3";;
-let testMerge = mergeNew (string2Type s11) (string2Type s12)
-printfn "\nTest Merge %s" (type2string (testMerge));;
-
-let s10 = "#1#2-3+2+22+13#12-9#2+3#12-4";;
-let s14 = "#4#5+3#3-4";;
-let testCanonical = Canonical (string2Type s14)
-
-let s15 = "+2";;
-let s16 = "#5:3#4:3";;
-let testJoinCommit = jc (string2Type s15) (string2Type s16)
-printfn "\nTest JoinCommit %s" (type2string (testJoinCommit));;
-///// End Main Function /////
-
-let pause () =  
-  match System.Diagnostics.Debugger.IsAttached with  
-  | true ->  
-      printfn "\nPress any key to continue."  
-      System.Console.ReadKey(true) |> ignore  
-  | false -> ()
-
-pause ()  
---
+++ TestFsharp/TestFsharp/TestTypeSystem.fs
@@ -0,0 +1,75 @@
+﻿module TestTypeSystem
+open CoreTypeSystem
+///// Test Mainfunction /////
+
+let rec getNextTag (lst1: TagSeq) : TagSeq =
+    match lst1 with
+    | [] -> []
+    | x::xs -> [fst x,1]
+
+let isTag tag = if (tag = '+' || tag = '-' || tag = '#' || tag = ':') then true else false
+let rec removeValue (chrlst:char list) =
+ match chrlst with
+ | [] -> []
+ | x::xs -> if (isTag x) then x::xs else removeValue xs
+
+let rec getValue (chrlst:char list) =
+ match chrlst with
+ | [] -> []
+ | x::xs -> if (isTag x) then [] else x::getValue xs
+
+let string2charlist (str:string) = [for c in str -> c]
+let rec charlist2stringlist (s:char list) = 
+ match s with 
+ | [] -> []
+ | x::xs -> x.ToString()::charlist2stringlist xs
+let rec stringToInt str = System.Int32.Parse(str |> String.concat "")
+let rec charListToInt str = System.Int32.Parse(charlist2stringlist(str) |> String.concat "")
+let rec charList2String s = charlist2stringlist(s) |> String.concat ""
+
+let rec charList2type (chrlst:char list) : TagSeq=
+ match chrlst with
+ | [] -> []
+ | '+'::xs -> (Tag.Plus, charListToInt(getValue xs)):: charList2type (removeValue xs)
+ | '-'::xs -> (Tag.Minus, charListToInt(getValue xs)):: charList2type (removeValue xs)
+ | '#'::xs -> (Tag.Max, charListToInt(getValue xs)):: charList2type (removeValue xs)
+ | ':'::xs -> (Tag.Join, charListToInt(getValue xs)):: charList2type (removeValue xs)
+ | x::xs -> charList2type xs
+
+let rec type2string (lst:TagSeq) : string =
+ match lst with
+ | [] -> ""
+ | (Tag.Plus, n1)::xs -> "+" + n1.ToString() + type2string xs
+ | (Tag.Minus, n1)::xs -> "-" + n1.ToString() + type2string xs
+ | (Tag.Max, n1)::xs -> "#" + n1.ToString() + type2string xs
+ | (Tag.Join, n1)::xs -> ":" + n1.ToString() + type2string xs
+ | x::xs -> x.ToString() + type2string xs
+
+let rec string2Type str : TagSeq = charList2type (string2charlist str)
+
+///// Test /////
+let s13 = "-1#2-3+2+22-3-2";;
+printfn "Test Join %A" (type2string (join (string2Type s13)));
+
+let s11 = "#1#2:3";;
+let s12 = "#1#2:3";;
+let testMerge = mergeNew (string2Type s11) (string2Type s12)
+printfn "\nTest Merge %s" (type2string (testMerge));;
+
+let s10 = "#1#2-3+2+22+13#12-9#2+3#12-4";;
+let s14 = "#4#5+3#3-4";;
+let testCanonical = Canonical (string2Type s14)
+
+let s15 = "+2";;
+let s16 = "#5:3#4:3";;
+let testJoinCommit = jc (string2Type s15) (string2Type s16)
+printfn "\nTest JoinCommit %s" (type2string (testJoinCommit));;
+///// End Main Function /////
+
+let pause () =  
+  match System.Diagnostics.Debugger.IsAttached with  
+  | true ->  
+      printfn "\nPress any key to continue."  
+      System.Console.ReadKey(true) |> ignore  
+  | false -> ()
+pause ()  
