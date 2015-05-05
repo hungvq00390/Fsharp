ed0b5563c0883a3a8c357b48e5acdb913b177fed
hungvq00390 <hungvq00390@gmail.com>
2015-05-05 23:39:03 +0700

    Update Unit Test

--- TestFsharp/TestFsharp/TestTypeSystem.fs
+++ TestFsharp/TestFsharp/TestTypeSystem.fs
@@ -125,35 +125,43 @@ let strTest5 = "+1";;
 let strTest6 = "#2:2#3:2#4:2";;
 let checkJoinCommit = type2string (jc (string2Type strTest5) (string2Type strTest6)) |> should equal "#4#3:2#4:2"
 
+//+1-1 -> #1
+let ast1 = [Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1)]
+let checkAST1 = type2string (infer ast1 []) |> should equal "#1"
 
-//+1-1
-//let ast1 = [Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1)]
+//+1+1-1-1 -> #2
+let ast2 = [Leaf (Tag.Plus, 1); Leaf (Tag.Plus, 1);Leaf (Tag.Minus, 1);Leaf (Tag.Minus, 1)]
+let checkAST2 = type2string (infer ast2 []) |> should equal "#2"
 
-//+1+1-1-1
-//let ast1 = [Leaf (Tag.Plus, 1); Leaf (Tag.Plus, 1);Leaf (Tag.Minus, 1);Leaf (Tag.Minus, 1)]
+//+1(-1)-1 -> #2
+let ast3 = [Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)] 
+let checkAST3 = type2string (infer ast3 []) |> should equal "#2"
 
-//+1(-1)-1
-//let ast1 = [Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)] 
+//+1+1-1 (-1)-1 -> #2
+let ast4 = [Leaf (Tag.Plus, 1); Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1);Branch ([Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)] 
+let checkAST4 = type2string (infer ast4 []) |> should equal "#2"
 
-//+1+1-1 (-1)-1
-//let ast1 = [Leaf (Tag.Plus, 1); Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1);Branch ([Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)] 
+//+1(+1-1-1) -1 -> #3
+let ast5 = [Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)] 
+let checkAST5 = type2string (infer ast5 []) |> should equal "#3"
 
-//+1(+1-1-1) -1
-//let ast1 = [Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)] 
+//+2(+2-2-2) -2   -> #6
+let ast6 = [Leaf (Tag.Plus, 2); Branch ([Leaf (Tag.Plus, 2); Leaf (Tag.Minus, 2); Leaf (Tag.Minus, 2)]); Leaf (Tag.Minus, 2)] 
+let checkAST6 = type2string (infer ast6 []) |> should equal "#6"
 
-//+2(+2-2-2) -2
-//let ast1 = [Leaf (Tag.Plus, 2); Branch ([Leaf (Tag.Plus, 2); Leaf (Tag.Minus, 2); Leaf (Tag.Minus, 2)]); Leaf (Tag.Minus, 2)] 
-
-//+2(+1-1-1-1)-1-1
-//let ast1 = [Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1);Leaf (Tag.Minus, 1)] 
-
+//+2(+1-1-1-1)-1-1 -> #5
+let ast7 = [Leaf (Tag.Plus, 2); Branch ([Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1);Leaf (Tag.Minus, 1)] 
+let checkAST7 = type2string (infer ast7 []) |> should equal "#5"
 
 //+2(+1-1-1-1)+1(#2-1-1-1)-1#3-1#4-1
-let ast1 = [Leaf (Tag.Plus, 2); Branch ([Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Plus, 2); Leaf (Tag.Minus, 2); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1); Leaf (Tag.Plus, 3); Leaf (Tag.Minus, 3);Leaf (Tag.Minus, 1);Leaf (Tag.Plus, 4); Leaf (Tag.Minus, 4);Leaf (Tag.Minus, 1)] 
-let ast2 = [Leaf (Tag.Plus,1); Leaf (Tag.Plus,1); Leaf (Tag.Minus,1); Branch ([Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)]
+let ast8 = [Leaf (Tag.Plus, 2); Branch ([Leaf (Tag.Plus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Plus, 1); Branch ([Leaf (Tag.Plus, 2); Leaf (Tag.Minus, 2); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1); Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1); Leaf (Tag.Plus, 3); Leaf (Tag.Minus, 3);Leaf (Tag.Minus, 1);Leaf (Tag.Plus, 4); Leaf (Tag.Minus, 4);Leaf (Tag.Minus, 1)] 
+let checkAST8 = type2string (infer ast8 []) |> should equal "#11"
 
-printfn "infer 1: %A = %A" ast1 (infer ast1 []);;
-printfn "infer 2: %A = %A" ast2 (infer ast2 []);;
+let ast9 = [Leaf (Tag.Plus,1); Leaf (Tag.Plus,1); Leaf (Tag.Minus,1); Branch ([Leaf (Tag.Minus, 1)]); Leaf (Tag.Minus, 1)]
+let checkAST9 = type2string (infer ast9 []) |> should equal "#2"
+
+printfn "infer 1: %A = %A" ast8 (infer ast8 []);;
+printfn "infer 2: %A = %A" ast9 (infer ast9 []);;
 
 // Pause terminal
 let pause () =  
