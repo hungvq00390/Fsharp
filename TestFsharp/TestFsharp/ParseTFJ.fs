b2f228d08b5c91a37d7eacfed01c2b1f80968043
hungvq00390 <hungvq00390@gmail.com>
2015-05-08 19:28:32 +0700

    Refactor code

--- TestFsharp/TestFsharp/ParseTFJ.fs
+++ TestFsharp/TestFsharp/ParseTFJ.fs
@@ -8,7 +8,8 @@ let mutable countWork = 0;;
 let mutable tokenList = [""]
 
 let rec parseWorkTemp (h:int) (str: string list) : string list = 
-    if h > 0 then parseWorkTemp (h-1) (List.append (List.append ["+1"] str) ["-1"])
+    if h > 0 then 
+        parseWorkTemp (h-1) (List.append (List.append ["+1"] str) ["-1"])
     else str;;
 
 let parseWork (str: string list) : string list =
@@ -16,24 +17,30 @@ let parseWork (str: string list) : string list =
     parseWorkTemp countWork str
     
 let rec testLexerAndParserFromFile (fileName:string) = 
+    countWork <- 0
+    tokenList <- [""]
     use textReader = new System.IO.StreamReader(fileName)
     let lexbuf = LexBuffer<char>.FromTextReader textReader
     let mutable keepParsing = true
     while keepParsing = true do
-    let parseToTFJ = Parser.start Lexer.tokenstream lexbuf
-    if parseToTFJ = ["#"] then tokenList <- tokenList @ (parseWork [])
-        elif parseToTFJ <> [""] then tokenList <- tokenList @ (parseToTFJ)
-    if lexbuf.IsPastEndOfStream then keepParsing <- false
-
+        let parseToTFJ = Parser.start Lexer.tokenstream lexbuf
+        if parseToTFJ = ["#"] then 
+            tokenList <- tokenList @ (parseWork [])
+        elif parseToTFJ <> [""] then 
+            tokenList <- tokenList @ (parseToTFJ)
+        if lexbuf.IsPastEndOfStream then 
+            keepParsing <- false
 
 let getFile (fileName:string) = Path.Combine(__SOURCE_DIRECTORY__, fileName)
+
+// Define calculate from File
 let calculateTFJfromFile (fileName:string) = 
     let run = testLexerAndParserFromFile (getFile(fileName))
     let result = calculateTFJstring (tokenList |> String.concat "")
     printfn "Final result: %A" result
 
-
-let run = calculateTFJfromFile "test1.txt"
+let run = calculateTFJfromFile "test.txt"
+let run1 = calculateTFJfromFile "test1.txt"
 
 printfn "Press any key to continue..."
 System.Console.ReadLine() |> ignore
